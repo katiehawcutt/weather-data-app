8526b772c69d36176a416420400fdcad2d6188a2
vmilitaru <vmilitaru28@gmail.com>
2020-11-06 14:06:01 +0000

    wip

--- src/Components/display/index.js
+++ src/Components/display/index.js
@@ -5,9 +5,10 @@ function Display({weather}){
     
 return(
     <div className = "display">
-         <h1>{weather.name}</h1>
-        <h2>{weather.main.temp}</h2>
-        <h3>{weather.weather[0].description}</h3> 
+         <h1 className="city">{weather.name}</h1>
+        <h2 className="weather">{weather.main.temp}{" °C"}</h2>
+        <img/>
+        <h3 className="weather">{weather.weather[0].description}</h3> 
     </div>
 
 )

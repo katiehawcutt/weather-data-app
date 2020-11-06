5d2bfe21796e20c414c0201d828f05c1398a013e
Maxine Tillman <maxltillman@gmail.com>
2020-11-06 12:25:10 +0000

    fetch fixed

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,17 +2,14 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Display from "../display";
 
- 
 const api = {
   key: "64fb852b810905d928c26726c873e75f",
   base: "http://api.openweathermap.org/data/2.5/",
 };
 //http://maps.openweathermap.org/maps/2.0/weather/{op}/{z}/{x}/{y}&appid={API key}
 
-
-
 function App() {
-  const [weather, setWeather] = useState({});
+  const [weather, setWeather] = useState(null);
 
   useEffect(() => {
     async function fetchApi() {
@@ -20,22 +17,18 @@ function App() {
         `${api.base}weather?q=Birmingham&units=metric&APPID=${api.key}`
       );
       const data = await res.json();
-      setWeather(data);      
+      setWeather(data);
       console.log(data);
     }
     fetchApi();
-  },[]);
-
-
+  }, []);
 
   return (
     <div className="App">
       <header>
         <h1>Weather App</h1>
       </header>
-      <main>
-        <Display weather = {weather}/>
-      </main>
+      <main>{weather !== null && <Display weather={weather} />}</main>
     </div>
   );
 }
